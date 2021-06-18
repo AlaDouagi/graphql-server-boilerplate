5b02619cd5bd06ee75f470da71372393a3ec20ce
Ala Douagi <ala.douagi@gmail.com>
2021-06-18 14:42:01 +0100

    Add basic boilerplate code

---
+++ index.mjs
@@ -0,0 +1,61 @@
+import fastify from 'fastify';
+import { ApolloServer, gql } from 'apollo-server-fastify';
+
+const PORT = 4000;
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  logger: true,
+});
+
+const app = fastify();
+
+await server.start();
+
+console.log('Server started 🚀');
+
+app.register(server.createHandler());
+
+await app.listen();
+
+console.log(`App listening on port ${PORT} ✔️`);
